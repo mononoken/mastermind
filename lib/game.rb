fb04e932278f6c8642b7f7fd9699e3495ece89a3
mononoken <ken.maeshima@pm.me>
2022-03-03 16:50:28 -0600

    Set Game class up such that it initializes Codebreaker and Codemaker and start_game initializes the master_code.

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,19 @@
-require_relative 'code_combo'
+require_relative 'codemaker'
+require_relative 'codebreaker'
 
 class Game
+  attr_reader :codemaker
+
   def initialize
-    @codemaker = ComputerPlayer.new
-    @codebreaker = Player.new
+    @codemaker = Codemaker.new
+    @codebreaker = Codebreaker.new
+  end
+
+  def start_game
+    @codemaker.set_random_master_code
   end
 end
+
+new_game = Game.new
+new_game.start_game
+p new_game.codemaker.master_code.combo
