3d64593fc50e88c1a303ee659cc19afe3ae69d62
mononoken <ken.maeshima@pm.me>
2022-03-04 20:36:25 -0600

    Added Messagable to Game and setup play_game for rough game.

--- lib/game.rb
+++ lib/game.rb
@@ -1,19 +1,53 @@
 require_relative 'codemaker'
 require_relative 'codebreaker'
+require_relative 'messagable'
 
 class Game
+  include Messagable
   attr_reader :codemaker
 
   def initialize
     @codemaker = Codemaker.new
     @codebreaker = Codebreaker.new
+    @winner = nil
+    @round = 0
   end
 
-  def start_game
+  def play_game
+    puts welcome_msg
     @codemaker.set_random_master_code
+    self.play_round
+    until end_condition?
+      self.play_round
+    end
+  end
+
+  def play_round
+    @round += 1
+    p @codemaker.master_code
+    @codebreaker.set_guess_combo
+    if self.guess_correct?
+      @winner = @codebreaker
+      self.end_game
+    end
+    if @round >= 12
+      @winner = @codemaker
+      self.end_game
+    end
+  end
+
+  def guess_correct?
+    @codebreaker.guess_combo == @codemaker.master_code
+  end
+
+  def end_condition?
+    !@winner.nil? || @round >= 12
+  end
+
+  def end_game
+    puts "#{@winner.name} wins!"
   end
 end
 
 new_game = Game.new
-new_game.start_game
-p new_game.codemaker.master_code.combo
+new_game.play_game
