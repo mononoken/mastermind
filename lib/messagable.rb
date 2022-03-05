59536d7739e076b762fed98b429bc2d6a7fc2cf5
mononoken <ken.maeshima@pm.me>
2022-03-04 19:41:44 -0600

    Create messagable.rb with some messages for the game.

---
+++ lib/messagable.rb
@@ -0,0 +1,16 @@
+module Messagable
+  def welcome_msg
+    'Welcome! This is a game of Mastermind.'
+  end
+
+  def rules_msg
+    [
+      'Player is assigned to the role of Codebreaker.',
+      'The computer is the Codemaker.',
+      'The Codemaker will create a Master Code.',
+      'The Master Code will consist of an ordered sequence of four colors.',
+      'The Codebreaker will guess the Master Code with a total of 12 guesses.',
+      'The Codemaker will provide feedback after each guess the Codebreaker makes.'
+    ]
+  end
+end
