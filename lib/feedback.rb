c50053d542ecdc2d1e64d290a038dbc16386a710
mononoken <ken.maeshima@pm.me>
2022-03-23 21:50:40 -0500

    Fix Feedback so it does not count duplicate colors when reporting 'correct color only' feedback.

--- lib/feedback.rb
+++ lib/feedback.rb
@@ -6,16 +6,22 @@ class Feedback
     @master_code = master_code
     @correct_clr_pos = (0..3).count { |i| codebreaker_guess[i] == master_code[i] }
     codebreaker_guess_reduced =
-      (0..3).reduce([]) do |nonmatching, i| 
+      (0..3).reduce([]) do |nonmatching, i|
         nonmatching.push(codebreaker_guess[i]) if codebreaker_guess[i] != master_code[i]
         nonmatching
       end
     master_code_reduced =
-      (0..3).reduce([]) do |nonmatching, i| 
+      (0..3).reduce([]) do |nonmatching, i|
         nonmatching.push(master_code[i]) if codebreaker_guess[i] != master_code[i]
         nonmatching
       end
-    @correct_clr_only = codebreaker_guess_reduced.count{ |i| master_code_reduced.any?(i) }
+    @correct_clr_only = codebreaker_guess_reduced.count do |i|
+      contains_i = master_code_reduced.any?(i)
+      if contains_i
+        master_code_reduced.delete_at(master_code_reduced.index(i))
+      end
+      contains_i
+    end
   end
 
   def return_feedback
