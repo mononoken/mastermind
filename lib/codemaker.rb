3324208cd935231a22e4154f53eefaf0744adbe2
mononoken <ken.maeshima@pm.me>
2022-04-18 20:33:30 -0700

    Fix codemaker to check for proper input. However, this approach is not DRY.

--- lib/codemaker.rb
+++ lib/codemaker.rb
@@ -1,16 +1,41 @@
 require_relative 'code_combo'
+require_relative 'messagable'
 
 module Codemaker
   module HumanCodemaker
+    include Messagable
+
     def initialize
       super
       @master_code = nil
     end
 
+    # Duplicates from codebreaker.rb. There must be a better way to write these DRY.
+    def valid_code_length?(code)
+      code.count == 4
+    end
+
+    def invalid_code_length
+      puts 'You must choose 4 colors only.'
+      @colors = gets.chomp.downcase.split
+    end
+
+    def valid_colors?(colors)
+      colors.all? { |color| CodePeg.valid_colors.any?(color) }
+    end
+
+    def invalid_colors
+      puts 'Invalid input.'
+      puts explain_codebreaker_format
+      @colors = gets.chomp.downcase.split
+    end
+
     def set_master_code
       puts 'Set master code.'
-      colors = gets.chomp.downcase.split
-      @master_code = CodeCombo.new(*colors)
+      @colors = gets.chomp.downcase.split
+      invalid_code_length until valid_code_length?(@colors)
+      invalid_colors until valid_colors?(@colors)
+      @master_code = CodeCombo.new(*@colors)
     end
 
     def master_code
